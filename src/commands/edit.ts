ebaf414f4fc0b30799fa58b24a2fb7dc3535a07f
chris <chrissouth89@gmail.com>
2021-07-02 21:03:32 -0500

    error handling edit function

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -4,31 +4,35 @@ export const edit: CommandInt = {
   name: "edit",
   description: "Edit a previous post",
   run: async (message) => {
-    const { author, channel, content } = message;
-    const [, targetId, ...text] = content.split(" ");
+    try {
+      const { author, channel, content } = message;
+      const [, targetId, ...text] = content.split(" ");
 
-    const targetMessage = await channel.messages.fetch(targetId);
+      const targetMessage = await channel.messages.fetch(targetId);
 
-    if (!targetMessage) {
-      await channel.send("That is not a valid message ID.");
-      return;
+      if (!targetMessage) {
+        await channel.send("That is not a valid message ID.");
+        return;
+      }
+
+      const targetEmbed = targetMessage.embeds[0];
+
+      if (
+        targetEmbed.author?.name !==
+        author.username + "#" + author.discriminator
+      ) {
+        await channel.send(
+          "You are not the owner of this post, you cannot make edits to this post."
+        );
+        return;
+      }
+
+      targetEmbed.setDescription(text.join(" ").replace(/\\n/g, "\n"));
+
+      await targetMessage.edit(targetEmbed);
+      await message.delete();
+    } catch (err) {
+      console.log("edit command", err);
     }
-
-    const targetEmbed = targetMessage.embeds[0];
-
-    if (
-      targetEmbed.author?.name !==
-      author.username + "#" + author.discriminator
-    ) {
-      await channel.send(
-        "You are not the owner of this post, you cannot make edits to this post."
-      );
-      return;
-    }
-
-    targetEmbed.setDescription(text.join(" ").replace(/\\n/g, "\n"));
-
-    await targetMessage.edit(targetEmbed);
-    await message.delete();
   },
 };
